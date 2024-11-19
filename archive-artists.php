6927d2b546412899254de3786b6ec80833104a16
Lucifer <tim.noelmans@outlook.com>
2024-11-19 03:53:52 +0100

    feat(archive-artist): v1 done)

--- archive-artists.php
+++ archive-artists.php
@@ -2,33 +2,53 @@
 
 require_once get_template_directory() . '/partials/functions/page-starter.php';
 
-// $options = null;
+$options = [];
+$selectedGenre = null;
+$selectedOrder = "newest";
 
-// if (!empty($_GET['genre'])) {
-//     $selectedGenre = get_terms([
-//         'taxonomy' => 'genre',
-//         'slug' => $_GET['genre']
-//     ]);
+if (!empty($_GET['genre'])) {
+    $selectedGenre = get_term_by('slug', $_GET['genre'], 'genre');
 
-//     $options['tax_query'] = [
-//         [
-//             'taxonomy' => 'genre',
-//             'field' => 'term_id',
-//             'terms' => $selectedGenre[0]->term_id
-//         ]
-//     ];
-// }
+    if (!$selectedGenre) {
+        wp_redirect(get_post_type_archive_link('artists'));
+        exit;
+    }
 
-// if (!empty($_GET['order'])) {
-//     $selectedOrder = $_GET['order'];
-// }
+    $options['tax_query'] = [
+        [
+            'taxonomy' => 'genre',
+            'field' => 'id',
+            'terms' => $selectedGenre->term_id,
+            'hide_empty' => false
+        ]
+    ];
+}
 
-// $artists = get_posts([
-//     'post_type' => 'artists',
-//     'posts_per_page' => -1,
-//     'order' => $selectedOrder ?? 'ASC',
-//     $options
-// ]);
+if (!empty($_GET['order'])) {
+    switch ($_GET['order']) {
+        case 'oldest':
+            $options['orderby'] = 'date';
+            $options['order'] = 'ASC';
+            $selectedOrder = "oldest";
+            break;
+        case 'a-z':
+            $options['orderby'] = 'title';
+            $options['order'] = 'ASC';
+            $selectedOrder = "a-z";
+            break;
+        default:
+            $options['orderby'] = 'date';
+            $options['order'] = 'DESC';
+            $selectedOrder = "newest";
+            break;
+    }
+}
+
+$artists = get_posts([
+    'post_type' => 'artists',
+    'posts_per_page' => -1,
+    ...$options
+]);
 
 get_header();
 
@@ -37,8 +57,7 @@ ob_start();
 ?>
 
 <h1 class="font-heading text-heading-lg text-center md:text-heading-xl">
-    <!-- <?php // echo $selectedGenre[0]->name ?? "All"; ?> Artists -->
-    Work In Progress
+    <?php echo ucfirst($selectedGenre->name ?? "All"); ?> Artists
 </h1>
 
 <?php
@@ -47,12 +66,88 @@ pageStarter(ob_get_clean());
 
 ?>
 
-<section class="px-6 py-32">
+<section class="px-6 py-6">
 
-    <!-- <form action="#" aria-label="Filters">
-        <ul>
+    <form action="#" aria-label="Filters" class="max-w-7xl mx-auto">
+        <ul class="flex flex-col gap-3 md:flex-row md:gap-6 items-center">
+
+            <li class="hidden lg:block">
+                <fieldset class="[&_input]:hidden">
+                    <div class="flex justify-between items-center">
+                        <div class="flex gap-3 [&>label]:border-2 [&>label]:border-black [&>label]:flex [&>label]:justify-center [&>label]:items-center [&>label]:w-max h-12 [&>label]:px-6 [&>label]:rounded-md [&>label]:text-button-base genre-radio [&>label]:transition-colors [&>label]:duration-200 [&>label]:cursor-pointer">
+                            <label for="all">
+                                All
+                                <input type="radio" name="genre" id="all" value="" <?php if (!$selectedGenre) echo 'checked'; ?>>
+                            </label>
+                            <?php
+                            // Get 3 genres based on the number of artists in each genre
+                            $genres = get_terms([
+                                'taxonomy' => 'genre',
+                                'hide_empty' => false,
+                                'orderby' => 'count',
+                                'order' => 'DESC',
+                                'number' => 3
+                            ]);
+
+                            if ($selectedGenre) :
+                                ?>
+                                <label for="<?php echo $selectedGenre->slug; ?>">
+                                    <?php echo $selectedGenre->name; ?>
+                                    <input type="radio" name="genre" id="<?php echo $selectedGenre->slug; ?>" value="<?php echo $selectedGenre->slug; ?>" checked>
+                                </label>
+                                <?php
+                            endif;
+
+                            $foundDuplicate = false;
+                            foreach ($genres as $key => $genre) {
+                                if ($selectedGenre && $key == 2 && !$foundDuplicate) {
+                                    break;
+                                }
+
+                                if ($selectedGenre && $selectedGenre->slug == $genre->slug) {
+                                    $foundDuplicate = true;
+                                    continue;
+                                };
+                                ?>
+                                <label for="<?php echo $genre->slug; ?>">
+                                    <?php echo $genre->name; ?>
+                                    <input 
+                                        type="radio" 
+                                        name="genre" 
+                                        id="<?php echo $genre->slug; ?>" 
+                                        value="<?php echo $genre->slug; ?>" 
+                                        <?php if ($selectedGenre && $selectedGenre->slug == $genre->slug) echo 'checked'; ?>>
+                                </label>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </fieldset>
+            </li>
+
+            <script defer>
+
+            const genreRadio = document.querySelector('.genre-radio');
+
+            genreRadio.addEventListener('change', e => {
+                const url = new URL(window.location.href);
+
+                if (e.target.value === '') {
+                    url.searchParams.delete('genre');
+                } else {
+                    url.searchParams.set('genre', e.target.value);
+                }
+                
+                window.location.href = url.href;
+            });
+
+            </script>
+
+            <hr aria-hidden class="hidden lg:block border-none w-[2px] h-6 bg-neutral-200">
+
             <li 
-                class="*:stroke-neutral-600 flex px-6 py-3 gap-3 border border-neutral-600 rounded-md items-center focus-within:border-primary-300 relative"
+                class="*:stroke-neutral-600 flex px-6 gap-3 border border-neutral-600 rounded-md items-center focus-within:border-primary-300 relative h-12 w-full"
                 data-genre-input-container
             >
                 <svg fill="none" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -97,14 +192,23 @@ pageStarter(ob_get_clean());
             
             const genreContainer = document.querySelector('[data-genre-input-container]');
             const genreResultsContainer = genreContainer.querySelector('[data-genre-results-container]');
+            let genreIsOpen = false;
 
             // Focus the input when the container is clicked
             genreContainer.addEventListener('click', () => {
                 genreContainer.querySelector('input').focus();
             });
 
-            // hide/unhide the results container when the input is focused
-            genreContainer.addEventListener('focusin', () => {
+            // hide/unhide the results container
+            genreContainer.addEventListener('click', openGenreContainer);
+            genreContainer.addEventListener('focusin', openGenreContainer);
+            
+            function openGenreContainer() {
+                if (genreIsOpen) return;
+                window.addEventListener('click', windowClickHandler);
+                window.addEventListener('focusin', windowFocusHandler);
+                genreIsOpen = true;
+
                 genreResultsContainer.classList.remove('hidden');
 
                 setTimeout(() => {
@@ -112,24 +216,33 @@ pageStarter(ob_get_clean());
                     genreResultsContainer.style.transform = 'translateY(0.75rem)';
                     fetchAndDisplayGenres({ target: genreContainer.querySelector('input') });
                 }, 10);
-            });
+            }
 
-            genreContainer.addEventListener('focusout', () => {
+            function closeGenreContainer() {
+                if (!genreIsOpen) return;
+                genreIsOpen = false;
+                window.removeEventListener('click', windowClickHandler);
+                window.removeEventListener('focusin', windowFocusHandler);
+
+                genreResultsContainer.style.opacity = 0;
+                genreResultsContainer.style.transform = 'translateY(0)';
+
                 setTimeout(() => {
-                    
-                    if (genreContainer.contains(document.activeElement)) {
-                        return;
-                    }
+                    genreResultsContainer.classList.add('hidden');
+                }, 200);
+            }
 
-                    genreResultsContainer.style.opacity = 0;
-                    genreResultsContainer.style.transform = 'translateY(0)';
+            function windowClickHandler(e) {
+                if (!genreContainer.contains(e.target)) {
+                    closeGenreContainer();
+                }
+            }
 
-                    setTimeout(() => {
-                        genreResultsContainer.classList.add('hidden');
-                    }, 200);
-
-                }, 0);
-            });
+            function windowFocusHandler(e) {
+                if (!genreContainer.contains(e.target)) {
+                    closeGenreContainer();
+                }
+            }
 
             // Fetch genres from the API
             let timeout = null;
@@ -182,15 +295,121 @@ pageStarter(ob_get_clean());
                     if (data.length === 0) {
                         const li = document.createElement('li');
                         li.textContent = 'No genres found';
-                        li.classList.add('text-neutral-500', 'px-3', 'py-2');
+                        li.classList.add('text-neutral-500', 'px-6', 'py-2');
                         genreResultsContainer.querySelector('ul').appendChild(li);
                     }
                 }, 200);
             }
+
+            // accessibility
+
+            genreContainer.addEventListener('keydown', e => {
+                if (e.key === 'Escape') {
+                    const input = genreContainer.querySelector('input');
+
+                    if (input === document.activeElement) {
+                        input.blur();
+                        closeGenreContainer();
+                        return;
+                    }
+
+                    genreContainer.querySelector('input').focus();
+                }
+            });
+            </script>
+
+            <hr aria-hidden class="hidden md:block border-none w-[2px] h-6 bg-neutral-200">
+
+            <li class="w-full md:w-fit">
+                <fieldset class="[&_input]:hidden">
+                    <div class="flex justify-between items-center">
+                        <legend class="text-neutral-600 md:sr-only">Sort by</legend>
+                        <div 
+                            class="order-radio"
+                        >
+                            <label for="newest">
+                                Newest
+                                <input type="radio" name="order" id="newest" value="newest" <?php if ($selectedOrder == "newest") echo 'checked'; ?>>
+                            </label>
+                            <label for="oldest">
+                                Oldest
+                                <input type="radio" name="order" id="oldest" value="oldest" <?php if ($selectedOrder == "oldest") echo 'checked'; ?>>
+                            </label>
+                            <label for="a-z">
+                                A&#x2011;Z
+                                <input type="radio" name="order" id="a-z" value="a-z" <?php if ($selectedOrder == "a-z") echo 'checked'; ?>>
+                            </label>
+                        </div>
+                    </div>
+                </fieldset>
+            </li>
+
+            <script defer>
+            
+            const orderRadio = document.querySelector('.order-radio');
+
+            orderRadio.addEventListener('change', e => {
+                const url = new URL(window.location.href);
+                url.searchParams.set('order', e.target.value);
+                window.location.href = url.href;
+            });
+
             </script>
 
         </ul>
-    </form> -->
+    </form>
+
+
+    <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 my-12 max-w-7xl mx-auto">
+        <?php
+        foreach ($artists as $artist) :
+            $artistMeta = get_post_meta($artist->ID, 'artist', true);
+            $artistImage = get_the_post_thumbnail_url($artist->ID, 'medium');
+            $artistName = $artist->post_title;
+            $artistGenres = get_the_terms($artist->ID, 'genre');
+            $artistLink = get_the_permalink($artist->ID);
+            ?>
+            <li class="flex flex-col gap-6 justify-between">
+                <div class="flex flex-col gap-6">
+                    <a href="<?php echo $artistLink; ?>">
+                        <img src="<?php echo $artistImage; ?>" alt="<?php echo $artistName; ?>" class="w-full rounded-3xl shadow-card object-cover">
+                    </a>
+                    <div class="flex flex-col gap-3">
+                        <h2 class="font-heading text-heading-sm">
+                            <a href="<?php echo $artistLink; ?>"><?php echo $artistName; ?></a>
+                        </h2>
+                        <?php if ($artistGenres) : ?>
+                            <ul class="flex flex-wrap gap-3 items-center">
+                                <?php
+                                foreach ($artistGenres as $genre) :
+                                    ?>
+                                    <li>
+                                        <?php echo $genre->name; ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <a href="<?php echo $artistLink ?>" class="w-full black-button-colors flex gap-3 justify-center items-center text-button-base rounded-md h-12">
+                    Read more
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19 12L13 18M19 12L13 6M19 12L5 12" stroke="#FBFCFD" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" />
+                    </svg>
+                </a>
+            </li>
+        <?php 
+        endforeach; 
+        
+        if (empty($artists)) :
+            ?>
+            <li class="text-center text-neutral-600 py-48 col-span-full">
+                No artists found
+            </li>
+            <?php
+        endif;
+        ?>
 
 </section>
 
